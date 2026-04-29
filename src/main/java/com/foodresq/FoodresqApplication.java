6f82891f13a5005ecf69959db0357e1394016729
Tirca Teodora <mteodora.tirca@gmail.com>
2026-04-29 18:44:21 +0300

    First commit

---
+++ src/main/java/com/foodresq/FoodresqApplication.java
@@ -0,0 +1,13 @@
+package com.foodresq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodresqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodresqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodresq/FoodresqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodresq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodresqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
