4bfbb518951b1d26aa6fbc3d6934486802fec17b
Tirca Teodora <mteodora.tirca@gmail.com>
2026-05-17 17:03:47 +0300

    adding a fix for the forbidden 403 error

--- src/main/java/com/foodresq/config/SecurityConfig.java
+++ src/main/java/com/foodresq/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/error").permitAll()
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
